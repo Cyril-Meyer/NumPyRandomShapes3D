93559653646d988f9f1aa39a179b635d10dccc8f
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-02-12 08:30:40 +0100

    change example dimension to improve detection of bug

--- example.py
+++ example.py
@@ -4,5 +4,5 @@ import NumPyDraw.view as npdview
 
 print(nprs.dtype)
 
-X = nprs.random_shapes((128, 128, 128), shapes_number=16, shapes_max_size=(64, 64, 64), fill_range=(1, 4))
-npdview.show_stack(X, interval=25, axis=2, vmin=0, vmax=4)
+X = nprs.random_image((127, 128, 129), shapes_number=16, shapes_max_size=(63, 64, 65))
+npdview.show_stack(X, interval=25, axis=0, vmin=0, vmax=255)
