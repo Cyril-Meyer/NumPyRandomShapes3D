3a3d1939a7987d5985d31311381083b1c45788f6
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-02-12 07:43:20 +0100

    generate random image filled with random shapes

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ import numpy as np
 import nprs
 import NumPyDraw.view as npdview
 
-X = np.zeros((256, 256, 256), dtype=np.uint8)
-nprs.random_shapes(X, shapes_number=16, shapes_max_size=(128, 128, 128), fill_range=(1, 4))
-npdview.show_stack(X, interval=25, axis=2)
+print(nprs.dtype)
+
+X = nprs.random_shapes((128, 128, 128), shapes_number=16, shapes_max_size=(64, 64, 64), fill_range=(1, 4))
+npdview.show_stack(X, interval=25, axis=2, vmin=0, vmax=4)
--- nprs.py
+++ nprs.py
@@ -1,8 +1,21 @@
 from random import randint
 import numpy as np
-import skimage as sk
-import skimage.morphology
+import cv2
 import NumPyDraw.npd3d as npd3d
+
+dtype = np.uint8
+
+
+def random_spheroid(array_shape, shapes_max_size):
+    """
+    return an array filled with a random spheroid.
+    """
+    array = np.zeros(array_shape, dtype=dtype)
+    center = randint(0, array.shape[0]-1), randint(0, array.shape[1]-1), randint(0, array.shape[2]-1)
+    radius = randint(1, shapes_max_size[0]), randint(1, shapes_max_size[1]), randint(1, shapes_max_size[2]),
+    rotxy = np.deg2rad(randint(0, 90))
+    array[npd3d.spheroid_coordinate(array.shape, center, radius, rotxy)] = 1
+    return array
 
 
 def add_random_spheroid(array, shapes_max_size, fill):
@@ -16,19 +29,38 @@ def add_random_spheroid(array, shapes_max_size, fill):
     return array
 
 
-def random_shapes(array, shapes_number=1, shapes_max_size=(None, None, None), fill_range=(1, 1)):
+def random_shape(array_shape, shapes_max_size, iteration=10):
     """
-    return the given array filled with random shapes.
+    return an array filled with a random shape.
     """
-    sms = np.asarray(array.shape)
+    array = np.zeros(array_shape, dtype=dtype)
+    add_random_spheroid(array, shapes_max_size, 1)
+
+    # dilation and erosion with random structuring element to create random shapes from spheroid
+    for _ in range(iteration):
+        kernel = np.random.randint(2, size=(5, 5), dtype=np.uint8)
+        array = cv2.dilate(array, kernel, iterations=1)
+        kernel = np.random.randint(2, size=(5, 5), dtype=np.uint8)
+        array = cv2.erode(array, kernel, iterations=1)
+
+    return array
+
+
+def random_shapes(array_shape, shapes_number=1, shapes_max_size=(None, None, None), fill_range=(1, 1)):
+    """
+    return an array filled with random shapes.
+    """
+    array = np.zeros(array_shape, dtype=dtype)
+
+    sms = np.asarray(array_shape)
     for i in range(len(shapes_max_size)):
         if shapes_max_size[i] is not None:
-            sms[i] = min(shapes_max_size[i], array.shape[i])
+            sms[i] = min(shapes_max_size[i], array_shape[i])
     shapes_max_size = tuple(sms)
 
     for _ in range(shapes_number):
         fill = randint(fill_range[0], fill_range[1])
-        array = add_random_spheroid(array, shapes_max_size, fill)
+        mask = random_shape(array.shape, shapes_max_size)
+        array[mask == 1] = fill
 
     return array
-
