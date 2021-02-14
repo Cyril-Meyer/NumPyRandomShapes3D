4a4f665431abff1f5688225a3d8965cd127ee45f
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-02-14 21:28:53 +0100

    update example.py for better example gif

--- example.py
+++ example.py
@@ -7,12 +7,24 @@ import NumPyDraw.view as npdview
 print(nprs.dtype)
 
 t0 = time.time()
-# X = nprs.random_spheroid((64, 64, 64), (32, 32, 32))*255
-# X = nprs.random_shape_multi_resolution((256, 256, 256), shapes_max_size=(128, 128, 128))*255
-# X = nprs.random_shapes((64, 64, 64), shapes_number=16, shapes_max_size=(32, 32, 32), fill_range=(0, 255))
-X = nprs.random_image((128, 256, 256), shapes_number=64, shapes_max_size=(64, 128, 128))
+RSpheroid = nprs.random_spheroid((64, 64, 64), (32, 32, 32))*255
 t1 = time.time()
 print("time:", t1 - t0)
 
-npdview.show_stack(X, interval=25, axis=0, vmin=0, vmax=255)
-npdview.gif_stack(X, "example/random_3D_image.gif", interval=25, axis=0, vmin=0, vmax=255)
+t0 = time.time()
+RShapes = nprs.random_shapes((64, 64, 64), shapes_number=32, shapes_max_size=(32, 32, 32), fill_range=(0, 255))
+t1 = time.time()
+print("time:", t1 - t0)
+
+t0 = time.time()
+RImage = nprs.random_image((128, 128, 128), shapes_number=64, shapes_max_size=(64, 64, 64))
+t1 = time.time()
+print("time:", t1 - t0)
+
+npdview.show_stack(RSpheroid, interval=25, axis=0, vmin=0, vmax=255)
+npdview.show_stack(RShapes, interval=25, axis=0, vmin=0, vmax=255)
+npdview.show_stack(RImage, interval=25, axis=0, vmin=0, vmax=255)
+
+npdview.gif_stack(RSpheroid, "example/random_3D_spheroid.gif", interval=25, axis=0, vmin=0, vmax=255)
+npdview.gif_stack(RShapes, "example/random_3D_shapes.gif", interval=25, axis=0, vmin=0, vmax=255)
+npdview.gif_stack(RImage, "example/random_3D_image.gif", interval=25, axis=0, vmin=0, vmax=255)
